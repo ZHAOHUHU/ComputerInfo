a81492ac65e6f3587bca988866741cd7dafb6758
ZHAOHUHU <3066918701@qq.com>
2017-11-27 17:06:41 +0800

    回家接着来

--- src/main/java/teamway/shenzhen/tms9000/ComputerInfo.java
+++ src/main/java/teamway/shenzhen/tms9000/ComputerInfo.java
@@ -2,10 +2,20 @@ package teamway.shenzhen.tms9000;
 
 import java.io.File;
 import java.math.BigDecimal;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.file.Paths;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.hyperic.sigar.Cpu;
+import org.hyperic.sigar.CpuInfo;
+import org.hyperic.sigar.CpuPerc;
+import org.hyperic.sigar.FileSystemUsage;
 import org.hyperic.sigar.Mem;
+import org.hyperic.sigar.NetInfo;
+import org.hyperic.sigar.NetInterfaceConfig;
+import org.hyperic.sigar.NetInterfaceStat;
 import org.hyperic.sigar.Sigar;
 import org.hyperic.sigar.SigarException;
 
@@ -44,59 +54,150 @@ public class ComputerInfo {
 	/*
 	 * 总方法
 	 */
-	public Computer getComputerInfo() {
+	public Computer getComputerInfo(String macaddress,String diskName) {
 		
 
 		return new Computer();
 	}
 	/*
+	 * 本机的名字
+	 */
+	private String getname() {
+		try {
+			InetAddress addr = InetAddress.getLocalHost();
+			return addr.getHostName();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	/*
 	 * 获取CPU使用率
 	 * 
 	 */
-	private double getCpu() {
+	public double getCpu() {
+		try {
+			CpuPerc perc = sigar.getCpuPerc();
+			double d = perc.getCombined();
+			return d;
+		} catch (SigarException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return 0;
-		
-		
 	}
 	/*
 	 * 内存使用率的方法
 	 * 
 	 */
 	public double getMenmory() {
+		try {
+			Mem mem = sigar.getMem();
+			//内存总量
+			long total = mem.getTotal();
+			//已使用内存
+			long used = mem.getUsed();
+			double d=(double)used/total;
+			return d;
+			
+		} catch (SigarException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return 0;
 		
 	}
 	/*
 	 * 
-	 * 磁盘使用率
+	 * 磁盘使用率d:/
 	 * 
 	 */
-	public double getDisk() {
-		double totalHD = 0;
-		double usedHD = 0;
-		long allTotal = 0;
-		long allFree = 0;
-		for (char c = 'A'; c <= 'Z'; c++) {
-			String dirName = c + ":/";
-			File win = new File(dirName);
-			if (win.exists()) {
-				long total = (long) win.getTotalSpace();
-				long free = (long) win.getFreeSpace();
-				allTotal = allTotal + total;
-				allFree = allFree + free;
-			}
+	public double getDisk(String diskName) {
+		
+		try {
+			FileSystemUsage usage = sigar.getFileSystemUsage(diskName);
+			double d = usage.getUsePercent();
+			return d;
+		} catch (SigarException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		Double precent = (Double) (1 - allFree * 1.0 / allTotal) * 100;
-		BigDecimal b1 = new BigDecimal(precent);
-		precent = b1.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
-		return precent;
+		return 0;
 	}
+	/*
+	 * 自己设置定时器3秒去一次
+	 */
+	
+	//===========================================================================================
+	
+	
 	/*
 	 * 
 	 * 获取网络带宽的使用率
 	 */
-	private double getNet() {
-		return 0;
+	public void getNet(String macaddress) {
+		String name="";
+		try {
+			long receiveBytes1=0L;  
+		    long sendBytes1=0L;  
+		    long receiveBytes2=0L;  
+		    long sendBytes2=0L;  
+		    int speed=0; //网络带宽Mbps  
+			String[] ifNames = sigar.getNetInterfaceList();
+			
+			for(int i=0;i<+ifNames.length;i++) {
+				//name网卡名字
+				name=ifNames[i];
+				System.out.println(name);
+				NetInterfaceConfig ifconfig = sigar.getNetInterfaceConfig(name);
+				if(macaddress.equals(ifconfig.getHwaddr())) {
+					System.out.println(name);
+					break;
+				}
+			}
+			//获取网卡的状态对象io0
+			NetInterfaceStat stat = sigar.getNetInterfaceStat(name);
+			System.out.println(stat);
+			//获取接受的总字节数量
+			receiveBytes1=	stat.getRxBytes();
+			//获取发送的总字节数量
+			sendBytes1=stat.getTxBytes();
+			//总的通信字节数量
+			long totalBytes1=sendBytes1+receiveBytes1;
+			System.out.println(totalBytes1);
+			/*
+			 * 通过前一秒的通信字节数量和后
+			 * 一秒的通信字节数量的差值来计
+			 * 算该时刻的网络带宽 
+			 */
+			long startTime = System.currentTimeMillis();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			NetInterfaceStat stat2 = sigar.getNetInterfaceStat(name);
+			//获取接受的总字节数量
+			 receiveBytes2=	stat.getRxBytes();
+			//获取发送的总字节数量
+			sendBytes2=stat.getTxBytes();
+			//总的通信字节数量
+			long totalBytes2=sendBytes2+receiveBytes2;
+			System.out.println(totalBytes2);
+			//截至时间
+			long endTime = System.currentTimeMillis();
+			//两次时间差的字节数
+			long totalBytes=totalBytes2-totalBytes1;
+			double interval=(double)(endTime-startTime)/1000;
+			//网络带宽
+			double netspeed= (double)totalBytes*8/(1000000*interval);
+			System.out.println(netspeed);
+		} catch (SigarException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
---
+++ src/test/java/com/zhao/test/Student.java
@@ -0,0 +1,34 @@
+package com.zhao.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import teamway.shenzhen.tms9000.ComputerInfo;
+
+public class Student {
+	/*
+	 * 内存使用率
+	 */
+	@Test
+	public void test() {
+		ComputerInfo info = new ComputerInfo();
+         double d = info.getMenmory();
+         double disk = info.getDisk("d:/");
+         System.out.println("内存的利用率是："+d);
+         System.out.println("硬盘的利用率是："+disk);
+	}
+	/*
+	 * cpu使用率
+	 */
+	@Test
+	public void test1() {
+		ComputerInfo info = new ComputerInfo();
+		double d = info.getCpu();
+		System.out.println("CPU的利用率是："+d);
+//		double timeNet = info.getTimeNet("BC-AE-C5-6E-B8-FA");
+//		System.out.println("网络带宽的利用率是："+timeNet);
+	info.getNet("E0-05-C5-F1-6D-C0");
+	}
+
+}
